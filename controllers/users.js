f7ee3938fd92088e742fa04472cef9f3c083ed54
Akeem Dainty <adainty@businessinsider.com>
2021-01-18 04:08:10 -0500

    adds secret and json web token create function

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
-
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
+const { create } = require('../models/user');
 
 module.exports = {
     signup
@@ -10,8 +12,13 @@ module.exports = {
 async function signup(req, res) {
     try {
         const user = await User.create(req.body);
-        res.json({user})
+        const token = createJWT(user);
+        res.json({token});
     } catch(error){
         res.status(400).json({msg: 'bad request'});
     }
+}
+
+function createJWT(user){
+    return jwt.sign({user}, SECRET, {expiresIn: '24h'})
 }
