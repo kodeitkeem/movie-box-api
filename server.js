1f820b3dd76a19f2b8d532b0572ea18e24c6dbdd
Akeem Dainty <adainty@businessinsider.com>
2021-01-18 03:31:04 -0500

    installs and configs cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 require('dotenv').config();
 require('./config/database');
@@ -10,14 +11,14 @@ const app = express();
 // Middleware mount
 app.use(express.json());
 app.use(logger('dev'));
-
+app.use(cors());
 
 // Routes mount
-// app.use('/api/users', require('./routes/api/users'));
+app.use('/api/users', require('./routes/api/users'));
 
 const port = process.env.PORT || 3001;
 
 // Listen on port
 app.listen(port, function(){
     console.log(`Express is listening on port ${port}`)
-})
+}) 
